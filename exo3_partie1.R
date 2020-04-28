3f7dd2c4ae555e3030700474e90c88d556ac74b5
osbornn <63869900+osbornn@users.noreply.github.com>
2020-04-28 15:15:23 +0200

    Colori e presentazione
    cambiato la mappa un po

--- exo3_partie1.R
+++ exo3_partie1.R
@@ -2,31 +2,30 @@
 
 #Loading stuff
 library(leaflet)
-covid19 <- head(covid.19, 100)
 
 #Colors and shit
-mybins <- seq(4, 6.5, by=0.5)
-mypalette <- colorBin( palette="YlOrBr", domain=covid19$deces_total, na.color="transparent", bins=mybins)
+mybins <- seq(max(covid.19$deces_total), min(covid.19$deces_total), by=-2000)
+mypalette <- colorBin( palette = "RdYlGn", domain=covid.19$deces_total, na.color="transparent", bins=mybins)
 
 # Prepare the text for the tooltip:
 mytext <- paste(
-  "Décès totaux: ", covid19$deces_total, "<br/>", 
-  "Réanimations totales : ", covid19$reanimation_total, "<br/>", 
-  "Hospitalisés totaux : ", covid19$hospitalises_total, "<br/>",
-  "Guéris totaux : ", covid19$gueris_total, sep = "") %>%
+  "Décès totaux: ", covid.19$deces_total, "<br/>", 
+  "Réanimations totales : ", covid.19$reanimation_total, "<br/>", 
+  "Hospitalisés totaux : ", covid.19$hospitalises_total, "<br/>",
+  "Guéris totaux : ", covid.19$gueris_total, sep = "") %>%
   lapply(htmltools::HTML)
 
 #Map
-m <- leaflet(covid19) %>% 
+m <- leaflet(covid.19) %>% 
   addTiles()  %>% 
   setView( lat=-27, lng=170 , zoom=4) %>%
   addProviderTiles("Esri.WorldImagery") %>%
   addCircleMarkers(~longitude, ~latitude, 
-                  fillColor = ~deces_total, fillOpacity = 0.7, color="white", radius=8, stroke=FALSE,
+                  fillColor = ~mypalette(covid.19$deces_total), fillOpacity = 0.7, color="white", radius=8, stroke=FALSE,
                    label = mytext,
                    labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
   ) %>%
-  addLegend( pal=mypalette, values=~deces_total, opacity=0.9, title = "Magnitude", position = "bottomright" )
+  addLegend( pal=mypalette, values=~deces_total, opacity=0.9, title = "Nombre de morts", position = "bottomright" )
 
 m 
 
