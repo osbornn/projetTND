529902957dda57dae85eb32378ff2a7093f6c643
osbornn <63869900+osbornn@users.noreply.github.com>
2020-04-28 14:47:40 +0200

    j'ai ajouté des trucs frero
    mdr

--- exo3_partie1.R
+++ exo3_partie1.R
@@ -1,8 +1,12 @@
 #Question 3 Partie 1
 
+#Loading stuff
 library(leaflet)
-
 covid19 <- head(covid.19, 100)
+
+#Colors and shit
+mybins <- seq(4, 6.5, by=0.5)
+mypalette <- colorBin( palette="YlOrBr", domain=covid19$deces_total, na.color="transparent", bins=mybins)
 
 # Prepare the text for the tooltip:
 mytext <- paste(
@@ -13,16 +17,16 @@ mytext <- paste(
   lapply(htmltools::HTML)
 
 #Map
-m <- leaflet(covid.19) %>% 
+m <- leaflet(covid19) %>% 
   addTiles()  %>% 
   setView( lat=-27, lng=170 , zoom=4) %>%
   addProviderTiles("Esri.WorldImagery") %>%
   addCircleMarkers(~longitude, ~latitude, 
-                  fillOpacity = 0.7, color="white", radius=8, stroke=FALSE,
+                  fillColor = ~deces_total, fillOpacity = 0.7, color="white", radius=8, stroke=FALSE,
                    label = mytext,
                    labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
   ) %>%
-  #addLegend( pal=mypalette, values=~mag, opacity=0.9, title = "Magnitude", position = "bottomright" )
+  addLegend( pal=mypalette, values=~deces_total, opacity=0.9, title = "Magnitude", position = "bottomright" )
 
 m 
 
