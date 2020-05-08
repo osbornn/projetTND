f34c7511a0f14ba0d3c31b99900f9265469c2fba
Alessandro Rinaudo <>
2020-05-08 11:59:01 +0200

    aggiornamento script

--- automatic_commit.sh
+++ automatic_commit.sh
@@ -1,4 +1,13 @@
 git pull
 git add projet.Rmd
-git commit -m "ultimo aggiornamento del file del progetto"
+printf "Vuoi scrivere un messaggio particolare nel tuo commit? (s/n)\n"
+read -r messaggio
+if [ $messaggio == 's' ]
+then
+   printf "Scrivi qui sotto il tuo messaggio che apparirà nel commit \n"
+   read -r messaggioVeroEProprio
+   git commit -m "${messaggioVeroEProprio}"
+else
+   git commit -m "ultimo aggiornamento del file del progetto"
+fi
 git push
